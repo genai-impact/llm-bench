e03e2bb4e469b5483d906a79175908bb892b761c
Samuel Rince <s@rince.me>
2024-12-01 16:01:46 +0100

    feat: tests with awq and gptq

--- optimum/init.sh
+++ optimum/init.sh
@@ -1,6 +1,15 @@
 # Install optimum-benchmark
 python3 -m venv .venv
 source .venv/bin/activate
-pip install huggingface_hub==0.23.2
-pip install codecarbon==2.7.2
+pip install huggingface_hub
+pip install codecarbon
 pip install git+https://github.com/huggingface/optimum-benchmark.git
+
+# For AWQ
+#pip install autoawq[kernels]
+
+# For GPTQ
+#pip install optimum
+#pip install -U "numpy<2" 
+#pip install torch==2.2.1 --index-url https://download.pytorch.org/whl/cu121
+#pip install auto-gptq --no-build-isolation
